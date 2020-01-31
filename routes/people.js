708ff5ec309616d69d566c56c3ef5f938e6df2f3
anamika8 <anamikaeight@gmail.com>
2020-01-30 19:20:04 -0800

    added json format in response

--- routes/people.js
+++ routes/people.js
@@ -5,19 +5,15 @@ const request = require("request");
 /* GET /people/:id */
 router.get("/:id", function(req, res, next) {
   //res.send('enter resource here');
-  if (!req.params.id) {
-    const message = `Please provide resource id to get specific people details`;
-    console.error(message);
-    return res.status(404).send(message);
-  }
+  res.setHeader("Content-Type", "application/json");
 
   request.get(
     { url: `https://swapi.co/api/people/${req.params.id}` },
     (error, response, body) => {
       if (error) {
-        return res.status(500).json(error);
+        return res.status(500).send(error);
       }
-      res.status(response.statusCode).json(body);
+      res.status(response.statusCode).send(body);
     }
   );
 });
