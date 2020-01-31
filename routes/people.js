e66c4ed0f0f6af8084f1ff10dc744fde0b548a0c
anamika8 <anamikaeight@gmail.com>
2020-01-30 16:52:53 -0800

    added Url to fetch data

--- routes/people.js
+++ routes/people.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const request = require("request");
 
 /* GET /people/:id */
-router.get('/:id', function(req, res, next) {
-  res.send('enter resource here');
+router.get("/:id", function(req, res, next) {
+  //res.send('enter resource here');
+
+  if (!req.params.id) {
+    const message = `Please provide resource id to get specific people details`;
+    console.error(message);
+    return res.status(500).send(message);
+  }
+
+  request.get(
+    { url: `https://swapi.co/api/people/${req.params.id}` },
+    (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        res.json(body);
+      }
+    }
+  );
 });
 
 module.exports = router;
