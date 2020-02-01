c5d9aa14495474fcb7e567479c42a9ebf3cb016b
anamika8 <anamikaeight@gmail.com>
2020-01-31 16:32:41 -0800

    using async and await

--- routes/people.js
+++ routes/people.js
@@ -6,12 +6,31 @@ const fetch = require("node-fetch");
 router.get("/:id", function(req, res, next) {
   //res.send('enter resource here');
   //res.setHeader("Content-Type", "application/json");
+  const peopleApiUrl = `https://swapi.co/api/people/${req.params.id}`;
 
-  let peopleInfo = {
-    name,
-    species
+  let fetchPeopleData = async function(url) {
+    let response = await fetch(url);
+    let peopleResults = await response.json();
+    let peopleName = peopleResults.name;
+    let speciesArray = peopleResults.species;
+    let species = [];
+    for (let i = 0; i < speciesArray.length; i++) {
+      const speciesApiUrl = speciesArray[i];
+      console.log(`The species url: ${speciesApiUrl}`);
+      let speciesResponse = await fetch(speciesApiUrl);
+      let speciesResult = await speciesResponse.json();
+      species.push(speciesResult.name);
+    }
+    let result = {
+      name: peopleName,
+      species: species.join()
+    };
+    return result;
   };
 
+  fetchPeopleData(peopleApiUrl).then(result => res.send(result));
+
+  /*
   fetch(`https://swapi.co/api/people/${req.params.id}`)
     .then(response => response.json())
     .then(responseJson => {
@@ -30,6 +49,7 @@ router.get("/:id", function(req, res, next) {
     console.log(peopleInfo);
     res.send(peopleInfo);
   }
+  */
 });
 
 module.exports = router;
