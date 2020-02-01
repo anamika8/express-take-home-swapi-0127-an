f34dc975a0f9969372d653290fd2606f74cdb4c5
anamika8 <anamikaeight@gmail.com>
2020-01-31 16:07:48 -0800

    testing in progress

--- routes/people.js
+++ routes/people.js
@@ -7,16 +7,28 @@ router.get("/:id", function(req, res, next) {
   //res.send('enter resource here');
   //res.setHeader("Content-Type", "application/json");
 
+  let peopleInfo = {
+    name,
+    species
+  };
+
   fetch(`https://swapi.co/api/people/${req.params.id}`)
     .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
+    .then(responseJson => {
+      peopleInfo = {
+        name: responseJson.name,
+        species: responseJson.species
+      };
+      return peopleInfo;
+    })
+    .then(peopleInfo => fetchSpeciesInfo(peopleInfo))
     .catch(error => {
       return res.status(500).send(error);
     });
 
-  function displayResults(responseJson) {
-    console.log(responseJson);
-    res.send(responseJson);
+  function fetchSpeciesInfo(peopleInfo) {
+    console.log(peopleInfo);
+    res.send(peopleInfo);
   }
 });
 
