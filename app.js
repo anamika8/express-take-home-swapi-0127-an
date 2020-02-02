a10f8cb08992a78b688c18c0d24996215b8a1d3c
anamika8 <anamikaeight@gmail.com>
2020-02-02 11:33:42 -0800

    added cors code

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  next();
+});
+
 app.use("/people", peopleRouter);
 app.use("/planets", planetRouter);
 
