67a3ddcfbf7a2063f00f5e7864be763f50acc9a8
anamika8 <anamikaeight@gmail.com>
2020-02-02 12:35:41 -0800

    added comments for people and planet routers

--- routes/people.js
+++ routes/people.js
@@ -7,21 +7,46 @@ router.get("/:id", function(req, res, next) {
   //res.send('enter resource here');
 
   const peopleApiUrl = `https://swapi.co/api/people/${req.params.id}`;
+  const logErrorMessage =
+    "Encountered the below error while fetching from the url:";
 
+  /**
+   * This method is going to call the SWAPI /people API
+   * and then call the subsequent /species API to get the
+   * species name of that person
+   *
+   * @param {*} url SWAPI /people with id as request param
+   */
   let fetchPeopleData = async function(url) {
-    let response = await fetch(url);
-    let peopleResults = await response.json();
+    let peopleResults = undefined;
+    try {
+      console.log(`Calling the /people API: ${url}`);
+      let response = await fetch(url);
+      peopleResults = await response.json();
+    } catch (error) {
+      console.log(`${logErrorMessage} ${url}`);
+      console.log(`ERROR: ${error}`);
+      return error;
+    }
+
     let peopleName = peopleResults.name;
     let speciesArray = peopleResults.species;
     let species = [];
     for (let i = 0; i < speciesArray.length; i++) {
       const speciesApiUrl = speciesArray[i];
-      console.log(`The species url: ${speciesApiUrl}`);
-      let speciesResponse = await fetch(speciesApiUrl);
-      let speciesResult = await speciesResponse.json();
-      species.push(speciesResult.name);
+      try {
+        console.log(`Calling the species url: ${speciesApiUrl}`);
+        let speciesResponse = await fetch(speciesApiUrl);
+        let speciesResult = await speciesResponse.json();
+        species.push(speciesResult.name);
+      } catch (error) {
+        console.log(`${logErrorMessage} ${speciesApiUrl}`);
+        console.log(`ERROR: ${error}`);
+        return error;
+      }
     }
 
+    // preparing the result to return in the expected format
     let result = {
       first_name: getFirstName(peopleName),
       last_name: getLastName(peopleName),
@@ -30,6 +55,10 @@ router.get("/:id", function(req, res, next) {
     return result;
   };
 
+  /**
+   * Utility method to extract the first name from the fullname
+   * @param {*} name
+   */
   function getFirstName(name) {
     let fullName = name.split(" ");
     let firstName = "";
@@ -39,6 +68,11 @@ router.get("/:id", function(req, res, next) {
     return firstName;
   }
 
+  /**
+   * Utility method to extract the last name from the fullname.
+   * If no last name, then returns undefined
+   * @param {*} name
+   */
   function getLastName(name) {
     let fullName = name.split(" ");
     let lastName = undefined;
@@ -48,6 +82,8 @@ router.get("/:id", function(req, res, next) {
     return lastName;
   }
 
+  // calling the method and sending the result back to the caller
+  // will be sending a 500 status code if any error is encountered
   fetchPeopleData(peopleApiUrl)
     .then(result => res.send(result))
     .catch(error => {
--- routes/planet.js
+++ routes/planet.js
@@ -5,19 +5,44 @@ const fetch = require("node-fetch");
 /* GET /planets/:id */
 router.get("/:id", function(req, res, next) {
   const planetApiUrl = `https://swapi.co/api/planets/${req.params.id}`;
+  const logErrorMessage =
+    "Encountered the below error while fetching from the url:";
 
+  /**
+   * This method is going to call the SWAPI /planets API
+   * and then call the subsequent /people and /species API to get the
+   * individual species count inside that planet
+   *
+   * @param {*} url SWAPI /planets with id as request param
+   */
   let fetchPlanetData = async function(url) {
-    let response = await fetch(url);
-    let planetResults = await response.json();
+    let planetResults = undefined;
+    try {
+      console.log(`Calling the /planets API: ${url}`);
+      let response = await fetch(url);
+      planetResults = await response.json();
+    } catch (error) {
+      console.log(`${logErrorMessage} ${url}`);
+      console.log(`ERROR: ${error}`);
+      return error;
+    }
+
     let planetName = planetResults.name;
     let planetResidents = planetResults.residents;
     let species = [];
     for (let i = 0; i < planetResidents.length; i++) {
       const peoplesApiUrl = planetResidents[i];
-      console.log(`The peoples url: ${peoplesApiUrl}`);
-      let peoplesResponse = await fetch(peoplesApiUrl);
-      let peoplesResult = await peoplesResponse.json();
-      species.push.apply(species, peoplesResult.species);
+      console.log(`Calling the /people API url: ${peoplesApiUrl}`);
+      try {
+        let peoplesResponse = await fetch(peoplesApiUrl);
+        let peoplesResult = await peoplesResponse.json();
+        // storing all species url in a single array instead of an array of arrays
+        species.push.apply(species, peoplesResult.species);
+      } catch (error) {
+        console.log(`${logErrorMessage} ${peoplesApiUrl}`);
+        console.log(`ERROR: ${error}`);
+        return error;
+      }
     }
 
     var getSpeciesInfo = async function(urls) {
@@ -26,22 +51,35 @@ router.get("/:id", function(req, res, next) {
         var data = await Promise.all(urls.map(requestSpeciesAsync));
         return data;
       } catch (err) {
-        console.error(err);
+        console.log(
+          `Encountered error fetching all /species info using Promise.all`
+        );
+        console.log(`ERROR: ${error}`);
+        return error;
       }
     };
 
     var requestSpeciesAsync = async function(url) {
-      let response = await fetch(url);
-      let speciesResult = await response.json();
-      return speciesResult;
+      try {
+        console.log(`Calling the /species API: ${url}`);
+        let response = await fetch(url);
+        let speciesResult = await response.json();
+        return speciesResult;
+      } catch (error) {
+        console.log(`${logErrorMessage} ${url}`);
+        console.log(`ERROR: ${error}`);
+        return error;
+      }
     };
 
+    // Storing all species data and counting the number of species
     speciesData = await getSpeciesInfo(species);
     let speciesName = [];
     speciesData.map(speciesInfo => {
       speciesName.push(speciesInfo.name);
     });
 
+    // creating an object to store the count of each species as a key-value pair
     let speciesCountObject = {};
     for (let i = 0; i < speciesName.length; i++) {
       if (speciesName[i] in speciesCountObject) {
@@ -51,6 +89,8 @@ router.get("/:id", function(req, res, next) {
       }
     }
 
+    // preparing the final result to be sent as per the expected format
+    console.log(`Preparing the final object to be sent as response`);
     let result = {
       name: planetName,
       count: speciesCountObject
